eab18a75f5b5b02f724bcbc8600edb27f34b57d3
Ranjan Rehman <Ranjan.Rehman@gmail.com>
2015-07-11 13:44:09 -0500

    Added a comment

--- app/src/main/java/com/example/ranjan/helloworld/MainActivity.java
+++ app/src/main/java/com/example/ranjan/helloworld/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.ranjan.helloworld;
 import android.app.Activity;
 import android.os.Bundle;
 
-
 public class MainActivity extends Activity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //just a stupid comment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
