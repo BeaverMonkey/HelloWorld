86f8409f8e592fc6fb31593092d77a5d7c2e8dcc
Ranjan Rehman <Ranjan.Rehman@gmail.com>
2015-07-11 13:50:30 -0500

    seriously stop!

--- app/src/main/java/com/example/ranjan/helloworld/MainActivity.java
+++ app/src/main/java/com/example/ranjan/helloworld/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity
     {
         //just a stupid comment
         //Just another stupid comment that will be a "mistake"
+        //Seriously stop!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
